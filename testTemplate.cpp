6cec03b66a1fab4773284acddc5847789233d00f
iffcharan <131170389+iffcharan@users.noreply.github.com>
2024-10-03 22:00:41 +0530

    Add files via upload

---
+++ testTemplate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pqi pair<int,int>
+#define vll vector<ll>
+void solve();
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+void solve()
+{
+
+}
