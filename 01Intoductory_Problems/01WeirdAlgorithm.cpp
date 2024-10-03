71bca8a1152c053a7b81ed514e5c33c5c60fb41f
iffcharan <131170389+iffcharan@users.noreply.github.com>
2024-10-03 22:03:17 +0530

    Update 01WeirdAlgorithm.cpp
    First file (03-10-2006)

--- 01Intoductory_Problems/01WeirdAlgorithm.cpp
+++ 01Intoductory_Problems/01WeirdAlgorithm.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define pii pair<int,int>
+#define pqi pair<int,int>
+#define vll vector<ll>
+void solve();
+int main()
+{
+    solve();
+    return 0;
+}
 
+void solve()
+{
+    ll n;
+    cin>>n;
+    while(n!=1)
+    {
+        cout<<n<<" ";
+        if(n&1) n = n*3 + 1;
+        else n /= 2;
+    }
+    cout<<n<<endl;
+}
